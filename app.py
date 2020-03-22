ebfb8bdef2c8b1bfbb202c5e12824f75352ea881
Simone Gaiarin <simgunz@gmail.com>
2020-03-22 16:09:59 +0100

    Add both fits, preserve visibility of traces

--- app.py
+++ app.py
@@ -127,9 +127,14 @@ app.layout = html.Div(
 def create_total_cases(selected_day_index, relayoutData, prev_figure):
     traces = []
     errors = []
+    visible_state = {}
     if prev_figure:
         # Set default xaxis range to existing one
         xaxis_range = prev_figure["layout"]["xaxis"]["range"]
+        # Get trace visibility state
+        for i in prev_figure["data"]:
+            visible = i["visible"] if "visible" in i.keys() else True
+            visible_state[i["name"]] = visible
     else:
         # Set xaxis defaults when figure created for the first time
         xmin = str(x_days[0] - timedelta(days=1))
@@ -182,61 +187,65 @@ def create_total_cases(selected_day_index, relayoutData, prev_figure):
         name="Data",
     )
 
-    if selected_day_index <= 18:
-        try:
-            p0 = (1, 1e-6, 1)
-            y_exp = fit_data(
-                exponenial_func,
-                x_days_index,
-                y_cases_total,
-                p0,
-                selected_day_index,
-                fit_day_count,
+    try:
+        p0 = (1, 1e-6, 1)
+        y_exp = fit_data(
+            exponenial_func,
+            x_days_index,
+            y_cases_total,
+            p0,
+            selected_day_index,
+            fit_day_count,
+        )
+        traces.append(
+            dict(
+                x=x_days,
+                y=y_exp,
+                # text=df_by_continent['country'],
+                mode="line",
+                line={"color": colors[2]},
+                opacity=1,
+                name="Exponential fit",
             )
-            traces.append(
-                dict(
-                    x=x_days,
-                    y=y_exp,
-                    # text=df_by_continent['country'],
-                    mode="line",
-                    line={"color": colors[2]},
-                    opacity=1,
-                    name="Exponential fit",
-                )
-            )
+        )
+        if (
+            "Exponential fit" not in visible_state
+            or visible_state["Exponential fit"] is True
+        ):
             yaxis_max = np.maximum(yaxis_max, y_exp[ridx])
-        except RuntimeError:
-            errors.append("Exponential fit failed")
-    else:
-        try:
-            p0 = (
-                max(y_cases_total),
-                np.median(np.arange(day_count)),
-                1,
-                min(y_cases_total),
+    except RuntimeError:
+        errors.append("Exponential fit failed")
+
+    try:
+        p0 = (
+            max(y_cases_total),
+            np.median(np.arange(day_count)),
+            1,
+            min(y_cases_total),
+        )
+        y_logi = fit_data(
+            logistic_func,
+            x_days_index,
+            y_cases_total,
+            p0,
+            selected_day_index,
+            fit_day_count,
+        )
+        traces.append(
+            dict(
+                x=x_days,
+                y=y_logi,
+                # text=df_by_continent['country'],
+                mode="line",
+                line={"color": colors[3]},
+                opacity=1,
+                name="Logistic fit",
             )
-            y_logi = fit_data(
-                logistic_func,
-                x_days_index,
-                y_cases_total,
-                p0,
-                selected_day_index,
-                fit_day_count,
-            )
-            traces.append(
-                dict(
-                    x=x_days,
-                    y=y_logi,
-                    # text=df_by_continent['country'],
-                    mode="line",
-                    line={"color": colors[3]},
-                    opacity=1,
-                    name="Logistic fit",
-                )
-            )
+        )
+        if "Logistic fit" not in visible_state or visible_state["Logistic fit"] is True:
             yaxis_max = np.maximum(yaxis_max, y_logi[ridx])
-        except RuntimeError:
-            errors.append("Logistic fit failed")
+    except RuntimeError:
+        errors.append("Logistic fit failed")
 
     traces += [data_used_for_fit, data_not_used_for_fit]
 
@@ -255,6 +264,12 @@ def create_total_cases(selected_day_index, relayoutData, prev_figure):
             legend={"x": 0.03, "y": 0.98},
         ),
     }
+
+    # Restore visibility state
+    for j in figure["data"]:
+        if j["name"] in visible_state:
+            j["visible"] = visible_state[j["name"]]
+
     return figure, "<br />".join(errors)
 
 
